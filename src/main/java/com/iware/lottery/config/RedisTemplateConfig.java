3cf8d44347c718c9b4556ea702e3f8f4b52c8286
johnma <372434120@qq.com>
2016-11-13 11:52:47 +0800

    初始化前台项目

--- src/main/java/com/iware/lottery/config/RedisTemplateConfig.java
+++ src/main/java/com/iware/lottery/config/RedisTemplateConfig.java
@@ -14,6 +14,7 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 @Configuration
 public class RedisTemplateConfig {
+
     @Bean
     JedisPoolConfig jedisPoolConfig(){
         JedisPoolConfig jpc =  new JedisPoolConfig();
